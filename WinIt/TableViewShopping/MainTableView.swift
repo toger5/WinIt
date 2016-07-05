13092f026f69a0d3e52e6a55e6aa08056fdfe2b3
toger5 <timo@Timos-MacBook-Pro.local>
2016-07-05 13:55:19 -0700

    first

---
+++ WinIt/TableViewShopping/MainTableView.swift
@@ -0,0 +1,9 @@
+//
+//  MainTableView.swift
+//  WinIt
+//
+//  Created by Timo on 03/07/16.
+//  Copyright © 2016 Timo. All rights reserved.
+//
+
+import Foundation
---
+++ WinIt/TableViewShopping/MainTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MainTableViewCell.swift
+//  WinIt
+//
+//  Created by Timo on 03/07/16.
+//  Copyright © 2016 Timo. All rights reserved.
+//
+
+import Foundation
