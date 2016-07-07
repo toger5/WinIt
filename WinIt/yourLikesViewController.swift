d4db54ba8c5a4d6e82b2d5a2cb6ea3db1386d671
toger5 <timo@Timos-MacBook-Pro.local>
2016-07-07 14:01:04 -0700

    added view controller table view for you likes

---
+++ WinIt/yourLikesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  WinIt
+//
+//  Created by Timo on 07/07/16.
+//  Copyright © 2016 Timo. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class yourLikesViewController: UIViewController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    
+    
+}
+extension yourLikesViewController: UITableViewDataSource{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
