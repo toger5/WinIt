cacbc312bfdb15c5edfe4530f41923d7779fa3fb
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-07-12 17:17:57 -0500

    Uncomment archive controller errors

--- WinIt/Archived/ArchivedPost.swift
+++ WinIt/Archived/ArchivedPost.swift
@@ -10,23 +10,23 @@ import UIKit
 import Firebase
 
 class ArchivedPost: Post{
-//    var participatedUsers: [String: (String,Int)] // dictionary with Keys and (names,Points)
-//    var winner: (String, String)
-//    var amountOfLikes: Int
-//
-//    override init(snapshot: FIRDataSnapshot) {
-//        self.participatedUsers = snapshot.value!["users"] as? [String: String] ?? ["nuUser": "test"]
-//        self.winner = snapshot.value!["winner"]
-//        self.amountOFLikes = snapshot.value!["amountOfLikes"]
-//        super.init(snapshot: snapshot)
-//    }
-//    
-//    init(post: Post, participatedUsers: [String: String], winner: (String, String)){
-//        self.participatedUsers = participatedUsers
-//        self.winner = winner
-//        super.init(post: post)
-//    }
-//    
-//    
+    var participatedUsers: [String: (String,Int)] // dictionary with Keys and (names,Points)
+    var winner: (String, String)
+    var amountOfLikes: Int
+
+    override init(snapshot: FIRDataSnapshot) {
+        self.participatedUsers = snapshot.value!["users"] as? [String: String] ?? ["nuUser": "test"]
+        self.winner = snapshot.value!["winner"]
+        self.amountOFLikes = snapshot.value!["amountOfLikes"]
+        super.init(snapshot: snapshot)
+    }
+    
+    init(post: Post, participatedUsers: [String: String], winner: (String, String)){
+        self.participatedUsers = participatedUsers
+        self.winner = winner
+        super.init(post: post)
+    }
+    
+    
     
 }
